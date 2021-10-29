d1edcdd5412fe9992d4b633c1c638fe0cb47b38d
bodeby <thorbensen@gmail.com>
2021-10-29 14:03:05 +0200

    added Greeting Route

--- prio/prio/src/main/java/com/skarp/prio/PrioApplication.java
+++ prio/prio/src/main/java/com/skarp/prio/PrioApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PrioApplication.class, args);
 	}
-
 }

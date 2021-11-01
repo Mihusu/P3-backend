e65007581e6404a9f96a5626a9a4be13e498f37d
bodeby <thorbensen@gmail.com>
2021-11-01 15:37:31 +0100

    Updated project

---
+++ prio/prio/src/main/java/com/skarp/prio/restservice/ProductController.java
@@ -0,0 +1,18 @@
+package com.skarp.prio.restservice;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private static final String template = "This is a, %s!";
+    private static int price = ThreadLocalRandom.current().nextInt(0,200);
+
+    @GetMapping("/product")
+    public Product product(@RequestParam(value = "name", defaultValue = "iPhone") String name) {
+        return new Product(String.format(template, name), price);
+    };
+}

e60d7e23d94923b8d83b3e5aabef6686c1037568
Sture <sturess@gmail.com>
2021-11-19 12:38:16 +0100

    Update PrioApplication.java

--- prio/prio/src/main/java/com/skarp/prio/PrioApplication.java
+++ prio/prio/src/main/java/com/skarp/prio/PrioApplication.java
@@ -2,7 +2,6 @@ package com.skarp.prio;
 
 import com.skarp.prio.products.Product;
 import com.skarp.prio.products.ProductRepository;
-import com.skarp.prio.repairs.Repair;
 import com.skarp.prio.repairs.RepairRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -31,19 +30,9 @@ public class PrioApplication implements CommandLineRunner {
 		repository.save(new Product("Macbook", Category.macbook, "M1", "2020", "16gb RAM", 8000, 9000));
 		repository.save(new Product("Apple", Category.iphone, "13 pro", "2021", "512gb", 11500, 10500));
 
-
 		System.out.println("All Products: ");
 		for (Product product : repository.findAll()) {
 			System.out.println(" - " + product.getName());
-		}
-
-		Product product = new Product("Lenovo", Category.laptop, "TI", "2016", "8gb RAM", 3000, 500);
-		repository.save(product);
-
-		repairRepository.save(new Repair(product));
-
-		for (Repair repair : repairRepository.findAll()) {
-			System.out.println(repair.getState());
 		}
 	}
 }

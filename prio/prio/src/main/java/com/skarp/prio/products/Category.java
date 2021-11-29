cb18117310d2f756d00a62c332b30768208265c1
DavidDHRasmussen <72596991+DavidDHRasmussen@users.noreply.github.com>
2021-11-29 15:25:26 +0100

    Javadoc documentation for: Product and category.

--- prio/prio/src/main/java/com/skarp/prio/products/Category.java
+++ prio/prio/src/main/java/com/skarp/prio/products/Category.java
@@ -1,6 +1,39 @@
 package com.skarp.prio.products;
+/**
+ * The {@code Category} enum stores the different electronic devices that a product could be.
+ * This information is stored in a {@link com.skarp.prio.products.Product Product}'s object.
+ *
+ *  These are the possible values:
+ *
+ *  <p>
+ *  - IPHONE
+ *  <p>
+ *  - MACBOOK
+ *  <p>
+ *  - IPAD
+ *  <p>
+ *  - LAPTOP
+ *  <p>
+ *  - SMARTPHONE
+ *  <p>
+ *  - TABLET
+ *  <p>
+ *
+ * This is an example of how we can set the  of electronic device of an already existing
+ * {@link com.skarp.prio.products.Product Product}'s object:
+ *
+ * <blockquote><pre>
+ *      Product product = new Product("productId");
+ *      product.setCategory(Category.IPHONE);
+ * </pre></blockquote>
 
+ *
+ * @author Team-Skarp
+ * @see com.skarp.prio.products
+ * @since ???
+ */
 public enum Category {
+    /** ??? */
     IPHONE,
     MACBOOK,
     IPAD,

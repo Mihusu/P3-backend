3d87e6c5c58932d02665090e318b6e6f82c6bf2b
Sture <sturess@gmail.com>
2021-12-02 12:49:31 +0100

    added delete parts from product when disapprove ticket

--- prio/prio/src/main/java/com/skarp/prio/user/UserRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.skarp.prio.user;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepositoryCustom{
-    User findByUsername(String username);
-
-}

840bb3befdbbcb28012f44f9c0e2c615f71e64e4
Hans Heje <hansheje@hotmail.com>
2021-12-03 12:00:49 +0100

    Created register & validateCookie

--- prio/prio/src/main/java/com/skarp/prio/user/UserController.java
+++ prio/prio/src/main/java/com/skarp/prio/user/UserController.java
@@ -47,7 +47,7 @@ public class UserController {
 
         if (username != null) { userQuery.addCriteria(Criteria.where("username").is(username)); }
 
-        try {
+
             // Find users with matching username
             User requestUser = operations.findOne(userQuery, User.class);
 
@@ -65,10 +65,92 @@ public class UserController {
             } else {
                 return new ResponseEntity<>("No such user", HttpStatus.NOT_FOUND);
             }
+        }
+
+
+    @PostMapping("/auth/validateSessionCookie")
+    public ResponseEntity<?> authenticateSessionCookie(
+            @RequestParam(value="username") String username,
+            @RequestParam(value="sessionCookie") String sessionCookie) {
+
+        Query userQuery = new Query();
+
+        if (username != null) { userQuery.addCriteria(Criteria.where("username").is(username)); }
+
+        try {
+            // Find users with matching username
+            User requestUser = operations.findOne(userQuery, User.class);
+
+            if (requestUser != null) {
+                // Check the sessionCookie against calculating what should be the correct sessionCookie
+                if (sessionCookie.equals(SHA3.hashPassword(requestUser.getCounter()+requestUser.getId()))) {
+                    return new ResponseEntity<>("Valid Cookie", HttpStatus.ACCEPTED);
+                } else {
+                    return new ResponseEntity<>("Invalid Cookie", HttpStatus.FORBIDDEN);
+                }
+            } else {
+                return new ResponseEntity<>("No such user", HttpStatus.NOT_FOUND);
+            }
         } catch (Exception e) {
             return new ResponseEntity<>(e, HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(
+            @RequestParam(value="username") String username,
+            @RequestParam(value="password") String password) {
+
+        try {
+            //check if the username is filled out
+            if (username.split("").length >= 4) {
+                //check if the username is already taken
+                Query userQuery = new Query();
+                userQuery = userQuery.addCriteria(Criteria.where("username").is(username));
+                if(!operations.exists(userQuery, User.class)){
+                    //check if the password livees up to the security requirements
+                    if(password.split("").length > 6){
+                        User user = new User(username, password);
+                        repository.save(user);
+                        return new ResponseEntity<>("User registered", HttpStatus.ACCEPTED);
+                    }else{return new ResponseEntity<>("Password to short", HttpStatus.NOT_ACCEPTABLE);}
+                }else{ return new ResponseEntity<>("Username already taken", HttpStatus.SERVICE_UNAVAILABLE); }
+            }else{ return new ResponseEntity<>("Username to short", HttpStatus.NOT_ACCEPTABLE);}
+        } catch (Exception e) {
+            return new ResponseEntity<>(e, HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+    /*
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(
+            @RequestParam(value="username") String username,
+            @RequestParam(value="password") String password) {
+        try {
+            Query userQuery = new Query();
+
+            //if username is not null
+            if (username != null) {
+                userQuery.addCriteria(Criteria.where("username").is(username));
+
+                //then Check if the username is legal, not legal if it already exists
+                User requestUser = operations.findOne(userQuery, User.class);
+                if (!requestUser.getUsername().equals(username)) {
+                    //check if the password is acceptable
+                    if (password.split("").length < 8) {
+                        User user = new User(username, password);
+                        repository.save(user);
+                        return new ResponseEntity<>("User registered", HttpStatus.ACCEPTED);
+                    } else {
+                        return new ResponseEntity<>("Password to short", HttpStatus.NOT_ACCEPTABLE);
+                    }
+                } else {
+                    return new ResponseEntity<>("Username already taken", HttpStatus.NOT_ACCEPTABLE);
+                }
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>(e, HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+     */
 }
 
 
